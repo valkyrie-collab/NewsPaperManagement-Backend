3d8b2b4392333f0c18c8cd7c265abebb04987a41
valkyrie <rajarshibiswas5678@gmail.com>
2025-10-09 18:46:39 +0000

    minor twicks in authentication

--- authentication/src/main/java/com/bytetrio/authentication/repository/UserRepository.java
+++ authentication/src/main/java/com/bytetrio/authentication/repository/UserRepository.java
@@ -1,16 +1,11 @@
 package com.bytetrio.authentication.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.bytetrio.authentication.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
-
-    @Query(value = "select 1 from users where username = :username", nativeQuery = true)
-    Integer checkIfUserPresent(@Param("username") String username);
 
 }
