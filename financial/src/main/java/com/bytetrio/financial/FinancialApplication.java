676ca4bff82bd87f19a33ec77b7a14757184f299
valkyrie <rajarshibiswas5678@gmail.com>
2025-10-21 11:50:35 +0000

    minor updated in docker

--- financial/src/main/java/com/bytetrio/financial/FinancialApplication.java
+++ financial/src/main/java/com/bytetrio/financial/FinancialApplication.java
@@ -2,8 +2,10 @@ package com.bytetrio.financial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class FinancialApplication {
 
 	public static void main(String[] args) {
--- financial/src/main/java/com/bytetrio/financial/config/CustomerFeignController.java
+++ financial/src/main/java/com/bytetrio/financial/config/CustomerFeignController.java
@@ -1,9 +1,11 @@
 package com.bytetrio.financial.config;
 
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@FeignClient("CUSTOMER")
 public interface CustomerFeignController {
     @GetMapping("/customer/check-customer-details")
     public ResponseEntity<Boolean> checkCustomerPresent(@RequestParam String token);
